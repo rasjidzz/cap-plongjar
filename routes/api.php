097f77105c73ba9f0ac2e96c61513fc97fbe9022
rasjidzz <mrisjads@gmail.com>
2025-04-13 15:58:01 +0700

    Initiate Laravel Project

--- routes/api.php
+++ routes/api.php
@@ -6,3 +6,13 @@ use Illuminate\Support\Facades\Route;
 Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('/', function () {
+    return ['Laravel' => app()->version()];
+});
+Route::get('/test', function () {
+    $data = [
+        'test' => 'test'
+    ];
+    return $data;
+});
