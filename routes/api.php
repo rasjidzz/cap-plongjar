26f0e5985a27579609938a313a4b4a51e4048055
rasjidzz <mrisjads@gmail.com>
2025-05-08 15:19:54 +0700

    Upd : Update DB and Sprint 5 PLG 34 (Add Mapping Kelas Matakuliah Role Prodi) 08/05/2025

--- routes/api.php
+++ routes/api.php
@@ -2,9 +2,11 @@
 
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\DosenController;
+use App\Http\Controllers\MappingKelasMatakuliahController;
 use App\Http\Controllers\MasterDataController;
 use App\Http\Controllers\MatakuliahController;
 use App\Http\Controllers\RoleController;
+use App\Http\Controllers\TahunAjaranController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -51,6 +53,8 @@ Route::prefix('masterdata')->group(function () {
     Route::middleware(['auth:sanctum', 'role:Superadmin,ProgramStudi'])->group(function () {
         Route::get('/getmappingkelasmatkulbyidmatkul/{id_matakuliah}', [MatakuliahController::class, 'getAllDosenByKKId']);
         Route::apiResource('matakuliahs', MatakuliahController::class);
+        Route::apiResource('tahunajarans', TahunAjaranController::class);
+        Route::apiResource('mappingkelasmatakuliahs', MappingKelasMatakuliahController::class);
     });
 });
 
