948d8bbb560d4c9a6f9e04cffeadf4d79db7d551
rasjidzz <mrisjads@gmail.com>
2025-06-21 11:30:51 +0700

    Upd : add api for testing the connection

--- routes/api.php
+++ routes/api.php
@@ -159,6 +159,8 @@ Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
     return $data;
 });
 
+Route::get('/testgetdatadosen', [DosenController::class, 'index']);
+
 // Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
 //     return $request->user();
 // });
